01b702ebf7f8004d70ed6069d270842a20887946
errornosignal <errornosignal@users.noreply.github.com>
2017-09-19 10:20:27 -0500

    init commit

--- Apple_HW2_HappyNumbers/main.swift
+++ Apple_HW2_HappyNumbers/main.swift
@@ -8,5 +8,51 @@
 
 import Foundation
 
-print("Hello, World!")
+//print program header
+print("HW1-RightTriangle\n")
+
+//get and validate user input
+func validateIntInput(prompt: String) -> Int? {
+    
+    while(true) {
+        print(prompt)
+        let inputString = readLine()!
+        
+        if ((NumberFormatter().number(from: inputString)?.intValue) != nil){
+            let intInput = Int(inputString)!
+            
+            if (inputString is Int) {
+                
+                let minValue = 1
+                let maxValue = 200
+            
+                if intInput >= minValue {
+                    if intInput <= maxValue {
+                        return intInput;
+                    }
+                    else {
+                        print("Error! Input is above '\(maxValue)' maximum value")
+                    }
+                }
+                else {
+                    print("Error! Input is below '\(minValue)' minimum value.")
+                    }
+            }
+        }
+            
+        else {
+            print("Error! Input is not a valid integer value from 1-200.")
+        }
+            
+    }
+}
+
+func isStringAnInt(string: String) -> Bool {
+    return Int(string) != nil
+}
+
+//set user input to variables
+let userInt = validateIntInput(prompt: "Enter a positive integer from 1-200:")
+
+print("\nuserInt = \(String(describing: userInt))\n")
 
